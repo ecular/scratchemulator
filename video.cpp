79fb0d30b8757d80584715f4bb8f7cd1e3e7c87b
ecular <ecular@bupt.edu.cn>
2014-03-26 22:10:14 +0800

    add video read function.not test

--- video.cpp
+++ video.cpp
@@ -9,7 +9,6 @@ video::video()
 
 void video::write_video(uint16_t port_num, uint8_t value)
 {
-    static uint8_t Table_Read_Step, Table_Write_Step; /*1:Red 2:Green 3:Blue*/
     static uint32_t Color_Data_Tmp;
     static uint16_t Cursor_Position;
     switch(port_num)
@@ -129,10 +128,71 @@ void video::write_video(uint16_t port_num, uint8_t value)
         }
         break;
     }
+
+    case(0x3DA):/*Feature control register.to init Attribute_Control_Reg_Flag.Maybe no use*/
+    {
+        Feature_Control_Reg = value & 0xFF;
+        Attribute_Control_Reg_Flag = 0;
+        break;
+    }
     }
 }
 
 uint8_t video::read_video(uint16_t port_num)
 {
-
+    switch(port_num)
+    {
+    case(0x3D5):/*CRT Control Reg*/
+    {
+        return CRT_Control_Reg[CRT_Control_Reg_Index];
+    }
+    case(0x3C5):/*Sequence Reg*/
+    {
+        return Sequence_Reg[Sequence_Reg_Index];
+    }
+    case(0x3CF):/*Graphic Control Reg*/
+    {
+        return Graphic_Control_Reg[Graphic_Control_Reg_Index];
+    }
+    case(0x3C0):/*Attribute Control Reg*/
+    {
+        return Attribute_Control_Reg[Attribute_Control_Reg_Index];
+    }
+    case(0x3C7):/*DAC status reg*/
+    {
+        return DAC_Status_Reg;
+    }
+    case(0x3C8):
+    {
+        return Table_Index_Reg_Write;
+    }
+    case(0x3C9):
+    {
+        switch(Table_Read_Step)
+        {
+        case(1):/*Red*/
+        {
+            Table_Read_Step++;
+            return Table_Data_Reg[Table_Index_Reg_Read] >> 2 & 0x3F;
+        }
+        case(2):/*Green*/
+        {
+            Table_Read_Step++;
+            return Table_Data_Reg[Table_Index_Reg_Read] >> 10 & 0x3F;
+        }
+        case(3):/*Blue*/
+        {
+            Table_Read_Step = 1;
+            Table_Index_Reg_Read++;
+            return Table_Data_Reg[Table_Index_Reg_Read] >> 18 & 0x3F;
+        }
+        }
+    }
+    case(0x3DA):/*Input status reg_0*/
+    {
+        Attribute_Control_Reg_Flag = 0;
+        return Input_Status_Reg0;
+    }
+    }
+    return 0;
 }
--- video.h
+++ video.h
@@ -5,13 +5,14 @@
 
 class video {
 private:
-    uint8_t CRT_Control_Reg[25], Sequence_Reg[5], Graphic_Control_Reg[9], Attribute_Control_Reg[20];
+    uint8_t CRT_Control_Reg[25], Sequence_Reg[5], Graphic_Control_Reg[9], Attribute_Control_Reg[20], Feature_Control_Reg, Input_Status_Reg0;
     uint32_t Table_Data_Reg[256];
     uint8_t CRT_Control_Reg_Index, Sequence_Reg_Index, Graphic_Control_Reg_Index, Attribute_Control_Reg_Index;
     uint8_t DAC_Status_Reg, Table_Index_Reg_Read, Table_Index_Reg_Write;
     uint8_t Attribute_Control_Reg_Flag;/*0:index.1:data*/
     uint16_t cursor_x, cursor_y;
     uint8_t columns, rows;
+    uint8_t Table_Read_Step, Table_Write_Step; /*1:Red 2:Green 3:Blue*/
 public:
     friend class Cpu;
     video();
