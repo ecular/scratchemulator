084bdf3a10f3cbd29472811cda578180f0889d05
ecular <ecular@bupt.edu.cn>
2014-03-28 17:14:26 +0800

    finish video write/read VGA_memory and interrupt()

--- video.cpp
+++ video.cpp
@@ -1,11 +1,66 @@
 #include "video.h"
 #include <stdint.h>
+#include <string.h>
 
-video::video()
+video::video(Cpu &cpu_arg): cpu(cpu_arg)
 {
+    for(int i = 0; i < 262144; ++i)
+        VGA_Memory[i] = 0;
     columns = 80;
     rows = 25;
+
+    width = 640;
+    high = 400;
+
+    CGApalette[0] = 0x000000; /*black*/
+    CGApalette[1] = 0x0000AA; /*blue*/
+    CGApalette[2] = 0x00AA00; /*green*/
+    CGApalette[3] = 0x00AAAA; /*cyan*/
+    CGApalette[4] = 0xAA0000; /*red*/
+    CGApalette[5] = 0xAA00AA; /*magenta*/
+    CGApalette[6] = 0xAA5500; /*brown*/
+    CGApalette[7] = 0xAAAAAA; /*light gray*/
+    CGApalette[8] = 0x555555; /*gray*/
+    CGApalette[9] = 0x5555FF; /*light blue*/
+    CGApalette[10] = 0x55FF55; /*light green*/
+    CGApalette[11] = 0x55FFFF; /*light cyan*/
+    CGApalette[12] = 0xFF5555; /*light red*/
+    CGApalette[13] = 0xFF55FF; /*light magenta*/
+    CGApalette[14] = 0xFFFF55; /*yellow*/
+    CGApalette[15] = 0xFFFFFF; /*white*/
 }
+inline uint8_t video::rotate_right(uint8_t value, uint8_t shift_bits)
+{
+    return (value >> shift_bits) | ((value << (8 - shift_bits)) & 0xFF);
+}
+
+inline uint8_t video::logic_operate(uint8_t value, uint8_t plane_lock)
+{
+    switch((Graphic_Control_Reg[3] >> 3) & 0x3)
+    {
+    case(0):/*driect return*/
+    {
+        return value;
+    }
+    case(1):/*AND*/
+    {
+        value &= plane_lock;
+        return value;
+    }
+    case(2):/*OR*/
+    {
+        value |= plane_lock;
+        return value;
+    }
+    case(3):/*XOR*/
+    {
+        value ^= plane_lock;
+        return value;
+    }
+    }
+    return value;
+}
+
 
 void video::write_video(uint16_t port_num, uint8_t value)
 {
@@ -195,4 +250,388 @@ uint8_t video::read_video(uint16_t port_num)
     }
     }
     return 0;
+}
+
+
+void video::write_video_memory(uint32_t addr_offset, uint8_t value)
+{
+    uint8_t write_value;
+    uint32_t plane_size = 0x10000;
+    switch(Graphic_Control_Reg[5] & 0x3)
+    {
+    case(0):/*write mode 0*/
+    {
+        /*rotate shift*/
+        value = rotate_right(value, Graphic_Control_Reg[3] & 0x7);
+
+        if(Sequence_Reg[2] & 0x1) /*Plane 0*/
+        {
+            if(Graphic_Control_Reg[1] & 0x1)
+            {
+                if(Graphic_Control_Reg[0] & 0x1)
+                    write_value = 0xFF;
+                else
+                    write_value = 0;
+            }
+            else
+            {
+                write_value = logic_operate(value, Plane_Lock[0]);
+            }
+            write_value = (Graphic_Control_Reg[8] & write_value) | (~Graphic_Control_Reg[8] & Plane_Lock[0]);
+            VGA_Memory[addr_offset] = write_value;
+        }
+
+        if(Sequence_Reg[2] & 0x2) /*Plane 1*/
+        {
+            if(Graphic_Control_Reg[1] & 0x2)
+            {
+                if(Graphic_Control_Reg[0] & 0x2)
+                    write_value = 0xFF;
+                else
+                    write_value = 0;
+            }
+            else
+            {
+                write_value = logic_operate(value, Plane_Lock[1]);
+            }
+            write_value = (Graphic_Control_Reg[8] & write_value) | (~Graphic_Control_Reg[8] & Plane_Lock[1]);
+            VGA_Memory[addr_offset + plane_size] = write_value;
+        }
+
+        if(Sequence_Reg[2] & 0x4) /*Plane 2*/
+        {
+            if(Graphic_Control_Reg[1] & 0x4)
+            {
+                if(Graphic_Control_Reg[0] & 0x4)
+                    write_value = 0xFF;
+                else
+                    write_value = 0;
+            }
+            else
+            {
+                write_value = logic_operate(value, Plane_Lock[2]);
+            }
+            write_value = (Graphic_Control_Reg[8] & write_value) | (~Graphic_Control_Reg[8] & Plane_Lock[2]);
+            VGA_Memory[addr_offset + plane_size * 2] = write_value;
+        }
+
+        if(Sequence_Reg[2] & 0x8) /*Plane 3*/
+        {
+            if(Graphic_Control_Reg[1] & 0x8)
+            {
+                if(Graphic_Control_Reg[0] & 0x8)
+                    write_value = 0xFF;
+                else
+                    write_value = 0;
+            }
+            else
+            {
+                write_value = logic_operate(value, Plane_Lock[3]);
+            }
+            write_value = (Graphic_Control_Reg[8] & write_value) | (~Graphic_Control_Reg[8] & Plane_Lock[3]);
+            VGA_Memory[addr_offset + plane_size * 3] = write_value;
+        }
+        break;
+    }
+
+    case(1):/*write mode 1*/
+    {
+        if(Sequence_Reg[2] & 0x1) /*Plane 0*/
+        {
+            VGA_Memory[addr_offset] = Plane_Lock[0];
+        }
+        if(Sequence_Reg[2] & 0x2) /*Plane 1*/
+        {
+            VGA_Memory[addr_offset + plane_size * 1] = Plane_Lock[1];
+        }
+        if(Sequence_Reg[2] & 0x4) /*Plane 2*/
+        {
+            VGA_Memory[addr_offset + plane_size * 2] = Plane_Lock[2];
+        }
+        if(Sequence_Reg[2] & 0x8) /*Plane 3*/
+        {
+            VGA_Memory[addr_offset + plane_size * 3] = Plane_Lock[3];
+        }
+        break;
+    }
+
+    case(2):/*write mode 2*/
+    {
+        if(Sequence_Reg[2] & 0x1) /*Plane 0*/
+        {
+            if(value & 0x1)
+                write_value = 0xFF;
+            else
+                write_value = 0;
+            logic_operate(write_value, Plane_Lock[0]);
+            write_value = (Graphic_Control_Reg[8] & write_value) | (~Graphic_Control_Reg[8] & Plane_Lock[0]);
+            VGA_Memory[addr_offset + plane_size * 0] = write_value;
+        }
+
+        if(Sequence_Reg[2] & 0x2) /*Plane 1*/
+        {
+            if(value & 0x2)
+                write_value = 0xFF;
+            else
+                write_value = 0;
+            logic_operate(write_value, Plane_Lock[1]);
+            write_value = (Graphic_Control_Reg[8] & write_value) | (~Graphic_Control_Reg[8] & Plane_Lock[1]);
+            VGA_Memory[addr_offset + plane_size * 1] = write_value;
+        }
+
+        if(Sequence_Reg[2] & 0x4) /*Plane 2*/
+        {
+            if(value & 0x4)
+                write_value = 0xFF;
+            else
+                write_value = 0;
+            logic_operate(write_value, Plane_Lock[2]);
+            write_value = (Graphic_Control_Reg[8] & write_value) | (~Graphic_Control_Reg[8] & Plane_Lock[2]);
+            VGA_Memory[addr_offset + plane_size * 2] = write_value;
+        }
+
+        if(Sequence_Reg[2] & 0x8) /*Plane 3*/
+        {
+            if(value & 0x8)
+                write_value = 0xFF;
+            else
+                write_value = 0;
+            logic_operate(write_value, Plane_Lock[3]);
+            write_value = (Graphic_Control_Reg[8] & write_value) | (~Graphic_Control_Reg[8] & Plane_Lock[3]);
+            VGA_Memory[addr_offset + plane_size * 3] = write_value;
+        }
+        break;
+    }
+    case(3):/*write mode 3*/
+    {
+        /*rotate shift*/
+        value = rotate_right(value, Graphic_Control_Reg[3] & 0x7);
+        uint8_t bit_mask = value & Graphic_Control_Reg[8];
+
+        if(Sequence_Reg[2] & 0x1) /*Plane 0*/
+        {
+            if(Graphic_Control_Reg[0] & 0x1)
+                write_value = 0xFF;
+            else
+                write_value = 0;
+            VGA_Memory[addr_offset + plane_size * 0] = write_value & bit_mask;
+        }
+
+        if(Sequence_Reg[2] & 0x2) /*Plane 1*/
+        {
+            if(Graphic_Control_Reg[0] & 0x2)
+                write_value = 0xFF;
+            else
+                write_value = 0;
+            VGA_Memory[addr_offset + plane_size * 1] = write_value & bit_mask;
+        }
+
+        if(Sequence_Reg[2] & 0x4) /*Plane 2*/
+        {
+            if(Graphic_Control_Reg[0] & 0x4)
+                write_value = 0xFF;
+            else
+                write_value = 0;
+            VGA_Memory[addr_offset + plane_size * 2] = write_value & bit_mask;
+        }
+
+        if(Sequence_Reg[2] & 0x8) /*Plane 3*/
+        {
+            if(Graphic_Control_Reg[0] & 0x8)
+                write_value = 0xFF;
+            else
+                write_value = 0;
+            VGA_Memory[addr_offset + plane_size * 3] = write_value & bit_mask;
+        }
+        break;
+    }
+    }
+}
+
+/*may have some error.may no use*/
+uint8_t video::read_video_memory(uint32_t addr_offset)
+{
+    uint32_t plane_size = 0x10000;
+    Plane_Lock[0] = VGA_Memory[addr_offset + plane_size * 0];
+    Plane_Lock[1] = VGA_Memory[addr_offset + plane_size * 1];
+    Plane_Lock[2] = VGA_Memory[addr_offset + plane_size * 2];
+    Plane_Lock[3] = VGA_Memory[addr_offset + plane_size * 3];
+    switch(Graphic_Control_Reg[4] & 0x3)
+    {
+    case(0):
+    {
+        Plane_Lock[0] = VGA_Memory[addr_offset + plane_size * 0];
+        return Plane_Lock[0];
+    }
+    case(1):
+    {
+        Plane_Lock[1] = VGA_Memory[addr_offset + plane_size * 1];
+        return Plane_Lock[1];
+    }
+    case(2):
+    {
+        Plane_Lock[2] = VGA_Memory[addr_offset + plane_size * 2];
+        return Plane_Lock[2];
+    }
+    case(3):
+    {
+        Plane_Lock[3] = VGA_Memory[addr_offset + plane_size * 3];
+        return Plane_Lock[3];
+    }
+    }
+    return 0;
+}
+
+void video::interrupt()
+{
+    switch(cpu.GetAH())
+    {
+    case(0):/*set video mode*/
+    {
+        switch(cpu.GetAL() & 0x10)
+        {
+        case(0):/*Text 40x25 16gray 0xB8000*/
+        {
+            Video_Buffer_Address = 0xB8000;
+            columns = 40;
+            rows = 25;
+            Colorful_Flag = 0;
+            Graphic_Mode_Flag = 0;/*text mode*/
+            for(uint32_t tmp_addr = Video_Buffer_Address; tmp_addr < Video_Buffer_Address + 0x4000; tmp_addr += 2)
+            {
+                cpu.ram[tmp_addr] = 0;/*ascii*/
+                cpu.ram[tmp_addr + 1] = 7;/*text attribute*/
+            }
+            break;
+        }
+
+        case(1):/*Text 40x25 16 fore/8 back  0xB8000*/
+        {
+            Video_Buffer_Address = 0xB8000;
+            columns = 40;
+            rows = 25;
+            Colorful_Flag = 1;/*colorful*/
+            Graphic_Mode_Flag = 0;/*text mode*/
+            for(uint32_t tmp_addr = Video_Buffer_Address; tmp_addr < Video_Buffer_Address + 0x4000; tmp_addr += 2)
+            {
+                cpu.ram[tmp_addr] = 0;/*ascii*/
+                cpu.ram[tmp_addr + 1] = 7;/*text attribute*/
+            }
+            break;
+        }
+
+        case(2):/*Text  80 x 25 16 gray  0xB8000*/
+        {
+            Video_Buffer_Address = 0xB8000;
+            columns = 80;
+            rows = 25;
+            Colorful_Flag = 0;/*colorful*/
+            Graphic_Mode_Flag = 0;/*text mode*/
+            for(uint32_t tmp_addr = Video_Buffer_Address; tmp_addr < Video_Buffer_Address + 0x4000; tmp_addr += 2)
+            {
+                cpu.ram[tmp_addr] = 0;/*ascii*/
+                cpu.ram[tmp_addr + 1] = 7;/*text attribute*/
+            }
+            break;
+        }
+
+        case(3):/*Text  80 x 25 16 fore/8 back  0xB8000*/
+        {
+            Video_Buffer_Address = 0xB8000;
+            columns = 80;
+            rows = 25;
+            Colorful_Flag = 1;/*colorful*/
+            Graphic_Mode_Flag = 0;/*text mode*/
+            for(uint32_t tmp_addr = Video_Buffer_Address; tmp_addr < Video_Buffer_Address + 0x4000; tmp_addr += 2)
+            {
+                cpu.ram[tmp_addr] = 0;/*ascii*/
+                cpu.ram[tmp_addr + 1] = 7;/*text attribute*/
+            }
+            break;
+        }
+
+        case(4):/*Text  80 x 25 16 fore/8 back  0xB8000*/
+        {
+            Video_Buffer_Address = 0xB8000;
+            columns = 80;
+            rows = 25;
+            Colorful_Flag = 1;/*colorful*/
+            Graphic_Mode_Flag = 0;/*text mode*/
+            for(uint32_t tmp_addr = Video_Buffer_Address; tmp_addr < Video_Buffer_Address + 0x4000; tmp_addr += 2)
+            {
+                cpu.ram[tmp_addr] = 0;/*ascii*/
+                cpu.ram[tmp_addr + 1] = 7;/*text attribute*/
+            }
+            break;
+        }
+
+        case(0x0D):/*320x200 16-color 0xA0000*/
+        case(0x12):/*640x480 16-color 0xA0000*/
+        case(0x13):/*320x200 256-color 0xA0000*/
+        {
+            Video_Buffer_Address = 0xA0000;
+            columns = 40;
+            rows = 25;
+            Colorful_Flag = 1;/*colorful*/
+            Graphic_Mode_Flag = 1;/*graphic mode*/
+            memset(cpu.ram + Video_Buffer_Address, 0, 0xFFFF);
+        }
+        }
+        Video_Mode = cpu.GetAL() & 0x10;
+        cursor_x = cursor_y = 0;
+        switch(Video_Mode)
+        {
+        case(0x12):
+        {
+            width = 640;
+            high = 480;
+            break;
+        }
+        case(0x0D):
+        case(0x13):
+        {
+            width = 320;
+            high = 200;
+            break;
+        }
+        default:
+        {
+            width = 640;
+            high = 400;
+        }
+        }
+        Mode_Change_Flag = 1;
+        break;
+    }
+
+    case(0x10):/*SET DAC REGISTER*/
+    {
+        switch(cpu.GetAL())
+        {
+        case(0x10):/*SET INDIVIDUAL DAC REGISTER (VGA/MCGA)*/
+        {
+            Table_Data_Reg[cpu.GetBX()] = (((cpu.GetDH() & 0x3F) << 2) | (((cpu.GetCL() & 0x3F) << 2) << 8) | (((cpu.GetCH() & 0x3F) << 2) << 16));
+            break;
+        }
+        case(0x12):/*SET BLOCK OF DAC REGISTERS (VGA/MCGA)*/
+        {
+            uint32_t start_index = cpu.GetBX();
+            uint32_t start_addr = (cpu.GetES() << 4) + cpu.GetDX();
+            for(uint32_t i = 0; i < cpu.GetCX(); ++i)
+            {
+                Table_Data_Reg[start_index + i] = (((cpu.ReadRam8(start_addr) & 0x3F) << 2) | (((cpu.ReadRam8(start_addr + 1) & 0x3F) << 2) << 8) | (((cpu.ReadRam8(start_addr + 2) & 0x3F) << 2) << 16));
+                start_addr += 3;
+            }
+            break;
+        }
+        }
+        break;
+    }
+    case(0x1A):/*GET DISPLAY COMBINATION CODE (PS,VGA/MCGA)*/
+    {
+        cpu.SetAL(0x1A);
+        cpu.SetBL(0x8);
+        break;
+    }
+    }
 }
